9dc595600fe0ae4726c66baae5609e40d54ba3a5
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2021-06-23 06:56:56 +0800

    Add files via upload

---
+++ Full Stack Cloud Development Capstone Project/functions/get-dealerships.js
@@ -0,0 +1,12 @@
+/**
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+ function main(params) {
+	return { message: 'Hello World' };
+}
---
+++ Full Stack Cloud Development Capstone Project/functions/post-review.py
@@ -0,0 +1,13 @@
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
+import sys
+
+def main(dict):
+    return { 'message': 'Hello world' }
